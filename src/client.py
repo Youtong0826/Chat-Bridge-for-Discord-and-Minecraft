81731310065074958e22f9d81982ae4c8eb49648
Youtong0826 <jimmy2007826@gmail.com>
2024-07-07 10:00:56 +0800

    add client type

---
+++ src/client.py
@@ -0,0 +1,9 @@
+from typing import Tuple, TypedDict
+from websocket_server import WebSocketHandler
+
+class WebSocketClient(TypedDict):
+    id: int
+    handler: WebSocketHandler
+    address: Tuple[str, int]
+    
+    
