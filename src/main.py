c65908023a2c4b0e6a1b337de8ab47c3208813f2
Youtong0826 <jimmy2007826@gmail.com>
2024-07-09 15:35:46 +0800

    merge

--- src/main.py
+++ src/main.py
@@ -1,48 +1,18 @@
-import logging
-import json
-
-from uuid import uuid4
 from os import getenv
 from dotenv import load_dotenv
-from discord import (
-    Bot,
-    Message,
-    Intents
-)
 
-from core.server import WebSocketServerThreading
-from core.setting import Setting
+from discord import Intents
+from bot import Bot
 
 load_dotenv()
 
 bot = Bot(intents=Intents.all())
-setting = Setting()
-websocket = WebSocketServerThreading(bot, setting.ws_setting["host"], setting.ws_setting["port"], logging.INFO)
+
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
-@bot.event
-async def on_message(msg: Message):
-    if msg.author.bot: return
-    
-    if msg.channel.id == setting.dc_setting.get("channel"):
-        websocket.client.send_message(json.dumps({  
-	        "header": {
-	        	"version": 1,
-                "requestId": str(uuid4()),
-	        	"messageType": "commandRequest",
-                "messagePurpose": "commandRequest"
-	        },
-            "body": {
-	        	"origin": {
-	        		"type": "player"
-	        	},
-	        	"commandLine": 'tellraw @a {"rawtext":[{"text": "%s"}]}' % f"§9[Discord] §e{msg.author.name}§r: {msg.content}",
-	        	"version": 1
-	        },
-        }))
-    
 if __name__ == "__main__":
-    websocket.start()
+    bot.load_extension("bot.message")
     bot.run(getenv("TOKEN"))
+    
