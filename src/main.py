d94e66db58427be09a83559b88948e89d7a0af35
Youtong0826 <jimmy2007826@gmail.com>
2024-07-17 08:33:52 +0800

    add logger

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import logging
 from os import getenv
 from dotenv import load_dotenv
 
@@ -6,11 +7,14 @@ from bot import Bot
 
 load_dotenv()
 
+logger = logging.getLogger("Discord")
+logger.setLevel(logging.INFO)
+
 bot = Bot(intents=Intents.all())
 
 @bot.event
 async def on_ready():
-    print(">> Bot is online <<")
+    logger.info(">> Bot is online <<")
 
 if __name__ == "__main__":
     bot.load_extension("bot.message")
